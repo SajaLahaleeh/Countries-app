f89b0bd66aa9f6322e79ae70143541ff95d6c383
yusralahleeh <yusra.lahaleeh@gmail.com>
2021-03-22 16:50:48 +0200

    Coubtries Angular Peoject

---
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -0,0 +1,16 @@
+// import { Component } from '@angular/core';
+// import { ThemeService } from 'src/app/services/theme.service';
+
+// @Component({
+//   selector: 'app-nav-bar',
+//   templateUrl: './nav-bar.component.html',
+//   styleUrls: ['./nav-bar.component.css']
+// })
+// export class NavBarComponent {
+//   private themeService: ThemeService
+//   constructor() {}
+
+//   toggleTheme() {
+//     this.themeService.toggleMode();
+//   }
+// }
